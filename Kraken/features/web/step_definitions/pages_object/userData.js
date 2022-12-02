a663632e5b449aa9339d4ce918a558b76d2f4270
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-12-02 08:53:55 -0500

    add kraken

---
+++ Kraken/features/web/step_definitions/pages_object/userData.js
@@ -0,0 +1,10 @@
+class UserData {
+    constructor() {
+        this.emailAdmin = 'c.barreiroh@uniandes.edu.co';
+        this.passwordAdmin = 'Q123456789';
+    }
+
+}
+module.exports = {
+    UserData,
+};
